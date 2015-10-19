3bf0d6d51ed13d69462e482d99501d1f368ffce4
chris <wfsr@foxmail.com>
2015-10-19 15:56:13 +0800

    add getHome and homeDirectory

--- test/lib/util.spec.js
+++ test/lib/util.spec.js
@@ -87,5 +87,105 @@ describe('util', function () {
         expect(util.typeOf(new Date())).toBe('date');
     });
     /* eslint-enable no-new-wrappers */
+});
 
+describe('Get user\'s home directory', function () {
+
+    var processObj = {
+        env: {
+            HOME: '',
+            LOGNAME: '',
+            USER: '',
+            LNAME: '',
+            USERNAME: '',
+            USERPROFILE: '',
+            HOMEDRIVE: '',
+            HOMEPATH: ''
+        },
+        platform: '',
+        getuid: null
+    };
+
+    describe('win32', function () {
+        var obj = util.mix(processObj, {platform: 'win32', env: {HOME: 'path/to/home/'}});
+
+        it('use env.HOME', function () {
+            expect(util.getHome(obj)).toBe(obj.env.HOME);
+        });
+
+        it('use env.USERPROFILE', function () {
+            obj.env.USERPROFILE = 'path/to/profile/';
+
+            expect(util.getHome(obj)).toBe(obj.env.USERPROFILE);
+        });
+
+        it('use env.HOMEDRIVE and env.HOMEPATH', function () {
+            obj.env.USERPROFILE = '';
+            obj.env.HOMEDRIVE = 'path/to/home-driver/';
+            obj.env.HOMEPATH = 'path/to/home-path/';
+
+            expect(util.getHome(obj)).toBe(obj.env.HOMEDRIVE + obj.env.HOMEPATH);
+        });
+
+    });
+
+    describe('darwin', function () {
+        var obj = util.mix(processObj, {platform: 'darwin', env: {HOME: 'path/to/home/'}});
+
+        it('use env.HOME', function () {
+            expect(util.getHome(obj)).toBe(obj.env.HOME);
+        });
+
+        it('use /Users/USERNAME', function () {
+            obj.env.HOME = '';
+            obj.env.USERNAME = 'chris';
+            expect(util.getHome(obj)).toBe('/Users/' + obj.env.USERNAME);
+        });
+
+        it('use /Users/LNAME', function () {
+            obj.env.HOME = '';
+            obj.env.LNAME = 'chris';
+            expect(util.getHome(obj)).toBe('/Users/' + obj.env.LNAME);
+        });
+
+        it('use /Users/USER', function () {
+            obj.env.HOME = '';
+            obj.env.USER = 'chris';
+            expect(util.getHome(obj)).toBe('/Users/' + obj.env.USER);
+        });
+
+        it('use /Users/LOGNAME', function () {
+            obj.env.HOME = '';
+            obj.env.LOGNAME = 'chris';
+            expect(util.getHome(obj)).toBe('/Users/' + obj.env.LOGNAME);
+        });
+    });
+
+    describe('linux', function () {
+        var obj = util.mix(processObj, {platform: 'linux', env: {HOME: 'path/to/home/'}});
+
+        it('use env.HOME', function () {
+            obj.env.HOME = 'path/to/home/';
+            expect(util.getHome(obj)).toBe(obj.env.HOME);
+        });
+
+        it('use /home/user', function () {
+            obj.env.HOME = '';
+            obj.getuid = function () {
+                return 1;
+            };
+
+            expect(util.getHome(obj)).toBe('/home/chris');
+        });
+
+        it('use /root', function () {
+            obj.env.HOME = '';
+            obj.getuid = function () {
+                return 0;
+            };
+
+            expect(util.getHome(obj)).toBe('/root');
+        });
+
+    });
 });
